9c38b5a49c26a80d436aab077e1779dd91b74365
XI ZHANG <x7zhang@gmail.com>
2018-10-11 17:55:53 -0400

    Add files via upload
    third day

--- src/containers/Virtualiz/rtm-container.js
+++ src/containers/Virtualiz/rtm-container.js
@@ -1,22 +1,108 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Button, Icon, Input } from 'antd';
 
 //import { getRTM, createNewRTM, deleteSpecificRTM } from '../../actions/virtual-action';
 //import { virtualService } from '../../services/virtual-service';
 import './virtualStyle/rmt.css';
 
+
+const Search = Input.Search;
+/*
+CreateRTM = (props) => {
+		console.log("createRTM clicked!!!");
+		return (
+			<div>
+			<button onKeyPress={props.nameRTM}>
+				<Icon type="edit" theme="outlined" onClick={this.createRTM}/>
+			</button>
+			<Search
+		      placeholder="Please enter your name"
+		      onSearch={value => console.log(value)}
+		      style={{ width: 200 }}
+		    />
+		    </div>
+			);
+ 
+	}
+	
+	DisplayRTM = (props) => {
+		const {projectedSize, allocatedSize, lems, dnaAmplification, status} = this.state;
+		return (
+			<div>
+			  <div>
+			     {projectedSize}
+			  </div>
+			  <div>
+			     availablesize
+			  </div>
+			  <div>
+			     numdisks
+			  </div>
+			  <div>
+			     nowrites
+			  </div>
+			  <div>
+			     online
+			  </div>
+			</div>
+		);
+	}
+	*/
+
+function Greeting() {
+	const rtmForm = false;
+
+		if (rtmForm) {
+			return (
+					<div className="RTMdisplay">
+			    <div>
+				  <div>
+				     projectedSize
+				  </div>
+				  <div>
+				     availablesize
+				  </div>
+				  <div>
+				     numdisks
+				  </div>
+				  <div>
+				     nowrites
+				  </div>
+				  <div>
+				     online
+				  </div>
+				</div>
+			  </div>
+		
+				)
+		}
+		else {
+			return (
+					<div className="nameEnter">
+			    <Search
+		      placeholder="Please enter your name"
+		      style={{ width: 200 }}
+		    	/>
+			  </div>
+				)
+		}
+					
+}
 class Rtm extends React.Component {
 	constructor(props) {
-		super(props);
+		super(props); // RTM_name === none ? create => active : getRTM()
 		this.state = {
 			projectedSize: 0,
 			allocatedSize: 0,
 			lems: 0,
 			dnaAmplification: 0,
-			status: null
+			status: null,
+			rtmName: null,
+			rtmForm: false,
 		}
-	}
+	};
 
 	componentDidMount() {
 		console.log("RTMMMM!!!!!!!!");
@@ -29,28 +115,29 @@ class Rtm extends React.Component {
 		  	this.setState
 		  	)
 		  	*/
+	};
+
+	keyPressCreateName = (e) => {
+		if (e.which === 13) { 
+			console.log("key presssssssssssss");
+			//this.displayRTM();
+		}
 	}
 
-	createRTM = () => {
-		console.log("createRTM clicked!!!");
+	
+
+	RTMTable = (props) => {
+		const rtmName = !props.rtmForm;
+		if (rtmName) {
+			return <createRTM />;
+		}
+		return <displayRTM/>
+
 	}
 	render() {
-		const {projectedSize, allocatedSize, lems, dnaAmplification, status} = this.state;
+		
 		return (
-			<div className="RTM">
-				<div className="form">
-				  <div className="mail">Create</div>
-				  <form action="">
-				    <input type="text" placeholder="Projected Size" />
-				    <input type="text" placeholder ="Allocated Size" />
-				    <input type="text" placeholder="LEMS" />
-				    <div className="rtmSubmit">Save</div>
-				  </form>
-				</div>
-				<div className="ok_message">
-					<span>Done!S</span>
-				</div>
-			</div>
+			<Greeting />
 			);
 	}
 };
